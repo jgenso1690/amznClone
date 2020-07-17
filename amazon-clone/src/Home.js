b82868bcfc7d692408c89e47e4ed4e21d3dabf35
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-07-17 14:53:54 -0400

    adding products

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './Product';
 import "./Home.css";
 
 function Home() {
@@ -6,8 +7,17 @@ function Home() {
         <div className="home">
             <img 
             className="home__image"
-            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt=""/>
-           
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt=""
+            />
+
+            <Product
+                id="123"                
+                title="JustRoomy Mid Century Modern Fabric Arm Chair"
+                price={149.99}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/716dBMnqlZL._AC._SR360,460.jpg"
+           />
+
         </div>
     )
 }
