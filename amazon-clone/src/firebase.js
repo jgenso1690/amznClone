ae16e87af1f98f3030a5f11b24277fd0fb1f5466
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-07-21 15:31:47 -0400

    sensitive info hidden

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
     
-        apiKey: "AIzaSyC7-bOsQY9-X0Lgdr6vdmqsYsD9MOOr1-w",
-        authDomain: "jimena-amznclone.firebaseapp.com",
-        databaseURL: "https://jimena-amznclone.firebaseio.com",
-        projectId: "jimena-amznclone",
-        storageBucket: "jimena-amznclone.appspot.com",
-        messagingSenderId: "66073307000",
-        appId: "1:66073307000:web:678b0d3a87443d5f764a8d",
-        measurementId: "G-HLKQ32PEJ3"
+        apiKey: process.env.DB_apiKey,
+        authDomain: process.env.DB_authDomain,
+        databaseURL: process.env.DB_databaseURL,
+        projectId: process.env.DB_projectId,
+        storageBucket: process.env.DB_storageBucket,
+        messagingSenderId: process.env.DB_messagingSenderId,
+        appId: process.env.DB_appId,
+        measurementId: process.env.DB_measurementId,
      
 });
 
